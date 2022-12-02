1dd06b3acbb45cf1100360e94e816298b9341b4e
hunter <flipmybit@protonmail.com>
2022-12-02 03:00:46 +0000

    check home for dummy button that grabs firebase uid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,34 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Button from "./Button"
 
+//testing dummy button
+import { getAuth } from "firebase/auth";
+
+
 export default function Home() {
+    let navigate = useNavigate();
     const handleLogout = () => {
         sessionStorage.removeItem('Auth Token');
         navigate('/login')
     }
-    let navigate = useNavigate();
+
+    const handleDummy = () => {
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+        if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    // ...
+           console.log(user.uid)
+        } else {
+            console.log("No user")
+    // No user is signed in.
+    }
+
+    }
+    //don't delete below comment, it stops the console from sreaming
+    // eslint-disable-next-line
     useEffect(() => {
         let authToken = sessionStorage.getItem('Auth Token')
 
@@ -18,10 +40,11 @@ export default function Home() {
         if (!authToken) {
             navigate('/login')
         }
-    })
+    }, [useNavigate])
     return (
         <div>
             <Button handleAction={handleLogout} title="Logout"></Button>
+            <Button handleAction={handleDummy} title="Dummy"></Button>
         </div>
     )
 }
